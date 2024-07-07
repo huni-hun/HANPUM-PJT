c8088520b91ea025213e0e1433b53812dbfe2af1
Taehun <t21212@naver.com>
2024-07-07 16:05:01 +0900

    [BE] create: 프로젝트 생성

---
+++ backend/src/main/java/backend/hanpum/HanpumApplication.java
@@ -0,0 +1,13 @@
+package backend.hanpum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanpumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanpumApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/backend/hanpum/HanpumApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.hanpum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanpumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
