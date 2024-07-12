498876a6aefe350b301527a067351d5dccb228af
rijfe <kyw42510@naver.com>
2024-07-12 20:53:35 +0900

    Feat: router 추가 및 세팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, {createGlobalStyle} from "styled-components";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import reset from "styled-reset";
+
+import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <GlobalStyle/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to={"/login"} replace={true}/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/main" element={<MainPage/>}/>
+        </Routes>
+      </Router>
+    </Container>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  html{
+    font-size: 10px;
+  }
+  *{
+    box-sizing: border-box;
+  }
+  body{
+    display: flex;
+  }
+`
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
