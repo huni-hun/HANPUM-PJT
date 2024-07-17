45d6c2970c6bffc555de160dcdcda80ac27cb413
오건영 <o54711254@gmail.com>
2024-07-17 20:34:39 +0900

    [BE]Feat: Schedule Entity 생성

---
+++ backend/src/main/java/backend/hanpum/domain/schedule/entity/Schedule.java
@@ -0,0 +1,40 @@
+package backend.hanpum.domain.schedule.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "schedule_id")
+    private Long id;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "date")
+    private String date;
+
+    @Builder.Default
+    @Column(name = "state")
+    private boolean state = false;
+
+    // Entity 생성 전까지 주석처리
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+
+  // 경로 하나에 일정이 여러개 있어야 할듯 -> member 컬럼이 있으니
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "course_id")
+//    private Course course;
+}
