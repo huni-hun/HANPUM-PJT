f676a06a7420d6861f17161d9a3910c8b9d49024
rijfe <kyw42510@naver.com>
2024-07-17 21:33:05 +0900

    [FE] Feat: Select 공용 컴포넌트 추가

---
+++ frontend/src/components/Select.tsx
@@ -0,0 +1,41 @@
+import * as S from "./Select.styled";
+
+interface SelectProps {
+  list: string[];
+  width: number;
+  height: number;
+  radius: number;
+  border: string;
+  fontSize: number;
+  fontColor: string;
+  isOpen: boolean;
+  padding: number;
+  setOpen: React.MouseEventHandler<HTMLDivElement>;
+  onClick: React.MouseEventHandler<HTMLLIElement>;
+}
+
+function Select(props: SelectProps) {
+  return (
+    <S.SelectBox
+      width={props.width}
+      height={props.height}
+      radius={props.radius}
+      border={props.border}
+      fontSize={props.fontSize}
+      fontColor={props.fontColor}
+      padding={props.padding}
+      onClick={props.setOpen}
+    >
+      <S.SelectLabel fontSize={props.fontSize} fontColor={props.fontColor}>
+        test
+      </S.SelectLabel>
+      <S.SelectList height={props.height} isOpen={props.isOpen} radius={props.radius} border={props.border}>
+        {props.list.map((ele) => (
+          <S.SelectItem onClick={props.onClick}>{ele}</S.SelectItem>
+        ))}
+      </S.SelectList>
+    </S.SelectBox>
+  );
+}
+
+export default Select;
