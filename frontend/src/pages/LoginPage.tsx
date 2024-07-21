c36c82c86f642fe7b15deb9b27aba1edc749320e
Sim <0doogadooga@gmail.com>
2024-07-21 12:25:48 +0900

    [FE] Modify: LoginPage에 있는 IconComponent Test 지우기

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,11 +1,7 @@
 import styled from "styled-components";
-import Icon from "../components/common/Icon/Icon";
 
 function LoginPage() {
-  return <LoginPageContainer>Login Page
-
-    <Icon name="IconNext" size={20}/>
-  </LoginPageContainer>;
+  return <LoginPageContainer>Login MainPage</LoginPageContainer>;
 }
 
 export default LoginPage;
