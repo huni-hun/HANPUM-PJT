a8936b393026041a49740908c73ace498a989ee2
Taehun <t21212@naver.com>
2024-07-22 23:40:50 +0900

    [BE]Feat: 에러코드, 성공코드 추가

--- backend/src/main/java/backend/hanpum/exception/format/response/ErrorCode.java
+++ backend/src/main/java/backend/hanpum/exception/format/response/ErrorCode.java
@@ -12,7 +12,11 @@ public enum ErrorCode {
     TEST_NOT_FOUND(HttpStatus.NOT_FOUND, "NOT FOUND 에러 예시용 테스트 코드 입니다."),
 
     /* 일정 */
-    SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "조회된 일정이 없습니다.")
+    SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "조회된 일정이 없습니다."),
+
+    /* 경로 */
+    COURSE_DETAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "조회된 경로 상세정보가 없습니다."),
+    COURSE_DAY_NOT_FOUND(HttpStatus.NOT_FOUND, "조회된 경로 일차정보가 없습니다.")
     ;
 
     private final HttpStatus status;
--- backend/src/main/java/backend/hanpum/exception/format/response/ResponseCode.java
+++ backend/src/main/java/backend/hanpum/exception/format/response/ResponseCode.java
@@ -12,7 +12,11 @@ public enum ResponseCode {
     TEST_SUCCESS(HttpStatus.OK, "성공 예시용 테스트 코드입니다."),
 
     /* 일정 */
-    SCHEDULE_LIST_FETCHED(HttpStatus.OK, "일정 정보가 성공적으로 조회되었습니다.")
+    SCHEDULE_LIST_FETCHED(HttpStatus.OK, "일정 정보가 성공적으로 조회되었습니다."),
+
+    /* 경로 */
+    COURSE_DETAIL_FETCHED(HttpStatus.OK, "경로 상세정보가 성공적으로 조회되었습니다."),
+    COURSE_DAY_FETCHED(HttpStatus.OK, "경로 일차 정보가 성공적으로 조회되었습니다.")
 
     ;
     private final HttpStatus status;
