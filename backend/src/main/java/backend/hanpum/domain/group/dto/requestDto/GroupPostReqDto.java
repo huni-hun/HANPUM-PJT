fd15c66da7cdd6359d721d31ea157871e0f7ff8b
seokchain <82chain@gmail.com>
2024-08-08 21:07:52 +0900

    [BE]Feat: 모임 생성 API 작성

---
+++ backend/src/main/java/backend/hanpum/domain/group/dto/requestDto/GroupPostReqDto.java
@@ -0,0 +1,20 @@
+package backend.hanpum.domain.group.dto.requestDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupPostReqDto {
+    private String title;
+    private String groupImg;
+    private String description;
+    private Integer recruitmentCount;
+    private Date recruitmentPeriod;
+}
