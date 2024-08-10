8a7bee08d910f6d724b5848cc221ed0e43b34c93
seokchain <82chain@gmail.com>
2024-08-10 21:55:17 +0900

    [BE]Feat: 회원정보 수정 메서드 추가

--- backend/src/main/java/backend/hanpum/domain/member/entity/Member.java
+++ backend/src/main/java/backend/hanpum/domain/member/entity/Member.java
@@ -84,4 +84,11 @@ public class Member {
     public void updateNickname(String nickname){
         this.nickname = nickname;
     }
+
+    public void updateMemberInfo(String name, Date birthDate, Gender gender, String phoneNumber){
+        this.name = name;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+    }
 }
