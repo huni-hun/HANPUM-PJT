7c58ecb34561efcb06492f37619b7443439e7d29
seokchain <82chain@gmail.com>
2024-08-18 15:02:58 +0900

    [BE]Modify: ResponseCode 오탈자 수정

--- backend/src/main/java/backend/hanpum/exception/format/response/ResponseCode.java
+++ backend/src/main/java/backend/hanpum/exception/format/response/ResponseCode.java
@@ -63,7 +63,7 @@ public enum ResponseCode {
     /* 멤버 */
     MEMBER_PROFILE_FETCHED(HttpStatus.OK, "회원 프로필이 성공적으로 조회되었습니다."),
     NICKNAME_UPDATE_SUCCESS(HttpStatus.OK, "닉네임이 성공적으로 변경되었습니다."),
-    PASSWORD_UPDATE_SUCCESS(HttpStatus.OK, "닉네임이 성공적으로 변경되었습니다."),
+    PASSWORD_UPDATE_SUCCESS(HttpStatus.OK, "비밀번호가 성공적으로 변경되었습니다."),
     MEMBER_INFO_UPDATE_SUCCESS(HttpStatus.OK, "회원정보가 성공적으로 변경되었습니다."),
     MEMBER_LIKE_GROUP_LIST_FETCHED(HttpStatus.OK, "관심 모임 리스트 조회가 성공적으로 조회되었습니다."),
     ;
