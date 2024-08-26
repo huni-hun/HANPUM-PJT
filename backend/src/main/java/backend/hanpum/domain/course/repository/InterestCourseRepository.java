3bef2799f1411bb3a7666620dd99fc5d15cfc49f
Taehun <t21212@naver.com>
2024-08-27 00:43:10 +0900

    [BE]Feat: 관심경로 목록 조회 서비스 추가

--- backend/src/main/java/backend/hanpum/domain/course/repository/InterestCourseRepository.java
+++ backend/src/main/java/backend/hanpum/domain/course/repository/InterestCourseRepository.java
@@ -4,7 +4,10 @@ import backend.hanpum.domain.course.entity.InterestCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InterestCourseRepository extends JpaRepository<InterestCourse, Long> {
     void deleteByMember_MemberIdAndCourse_CourseId(Long memberId, Long courseId);
+    List<InterestCourse> findByMember_MemberId(Long memberId);
 }
