cdbfb169d11eac66339b0949841972bac584c171
rijfe <kyw42510@naver.com>
2024-09-09 21:59:42 +0900

    [FE] Modify: 완주한 경로 표시

--- frontend/src/components/My/category/FinishedRoot.tsx
+++ frontend/src/components/My/category/FinishedRoot.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import * as S from '../../Style/My/category/FinishedRoot.styled';
 import RootCard from '../../RootCard';
+import { useQuery } from 'react-query';
+import { GetUseRouteList } from '@/api/mypage/GET';
+import { STATUS } from '@/constants';
+import { toast } from 'react-toastify';
+import { AxiosError } from 'axios';
 
 function FinishedRoot() {
+  const { data: finishedRoute } = useQuery('GetUseRouteList', GetUseRouteList, {
+    onSuccess: (res) => {
+      // console.log('res ::', res.data);
+      if (res.status === STATUS.success) {
+      } else if (res.status === STATUS.error) {
+        toast.error(res.message);
+      }
+    },
+    onError: (error: AxiosError) => {
+      toast.error(error.message);
+    },
+  });
   return (
     <S.FinishedRootContainer>
       <div className="card-list">
-        <RootCard />
-        <RootCard />
+        {finishedRoute &&
+          finishedRoute.data.map((ele: any) => <RootCard {...ele} />)}
       </div>
     </S.FinishedRootContainer>
   );
