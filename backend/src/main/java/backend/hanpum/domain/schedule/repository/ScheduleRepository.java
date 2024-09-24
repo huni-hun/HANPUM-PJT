a6e1d12e36843cbe8522bd523a2d4cffedb7454f
Taehun <t21212@naver.com>
2024-09-24 23:51:26 +0900

    [BE]Modify: 경로 삭제시 deleteState 변경

--- backend/src/main/java/backend/hanpum/domain/schedule/repository/ScheduleRepository.java
+++ backend/src/main/java/backend/hanpum/domain/schedule/repository/ScheduleRepository.java
@@ -4,6 +4,9 @@ import backend.hanpum.domain.schedule.entity.Schedule;
 import backend.hanpum.domain.schedule.repository.custom.ScheduleRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> , ScheduleRepositoryCustom {
     void deleteAllByMember_MemberId(Long memberId);
+    List<Schedule> findByCourse_courseId(Long courseId);
 }
